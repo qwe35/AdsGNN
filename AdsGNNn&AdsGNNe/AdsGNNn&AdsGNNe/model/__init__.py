55ecf89d87c45d2271713591d5fe7b7e9920ec39
qwe35 <73166711+qwe35@users.noreply.github.com>
2021-02-16 14:40:59 +0800

    Delete AdsGNNn&AdsGNNe/AdsGNNn&AdsGNNe directory

--- AdsGNNn&AdsGNNe/AdsGNNn&AdsGNNe/model/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: UTF-8 -*-
-from .model_edge_bert import ModelQKEdgeBert, ModelQKEdgeBertConfig, ModelQKEdgeBertWithTokenNet
-from .model_edge_transformer import ModelQKEdgeBERT, ModelQKEdgeBERTConfig
-from .model_token_net import TokenNetConfig, TokenNetBertModel
-from .model_node_level import BertNodeGnnConfig, BertNodeGnnModel
